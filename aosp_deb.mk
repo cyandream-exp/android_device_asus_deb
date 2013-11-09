2189bdb9abd7fd2feb3ead34bf5fe13c9a039a08
yanniks <kontakt@yanniks.de>
2013-11-09 22:04:20 +0100

    initial KiDream commit

--- aosp_deb.mk
+++ aosp_deb.mk
@@ -19,13 +19,14 @@
 
 # Inherit from hardware-specific part of the product configuration
 $(call inherit-product, device/asus/deb/device.mk)
+$(call inherit-product, vendor/kidream/configs/common.mk)
 
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 PRODUCT_NAME := aosp_deb
 PRODUCT_DEVICE := deb
-PRODUCT_BRAND := Android
-PRODUCT_MODEL := AOSP on Deb
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Nexus 7
 PRODUCT_MANUFACTURER := ASUS
-PRODUCT_RESTRICT_VENDOR_FILES := true
+PRODUCT_RESTRICT_VENDOR_FILES := false
